049f87d4709011fabe60b01c5b1e0d82acf252d2
Octavio Ruiz <oruizdeleon@ucsd.edu>
2017-10-31 13:46:57 -0700

    Return experiment date and time, file diagnostics, or rewrite a clean version of an E-Prime file

--- eprime_datetime_get.m
+++ eprime_datetime_get.m
@@ -1,8 +1,11 @@
-function [date,time,dtres,fres,nruns] = eprime_datetime_get(fname,varargin)
+function [date,time,dtres,fres] = eprime_datetime_get(fname,varargin)
 % function [date,time,dtres,fileres] = eprime_datetime_get(fname,varargin)
 %
 % Purpose: extract timestamp from eprime data file
-% Based on abcd_extract_eprime_mid.m
+% Based partly on abcd_extract_eprime_mid.m
+% 
+% Major modifications by Octavio Ruiz (2017sep-) to use his python script
+% to extract data from E-Prime spreadsheet files
 %
 % Input (required):
 %   fname: name of input eprime file
@@ -11,21 +14,21 @@ function [date,time,dtres,fres,nruns] = eprime_datetime_get(fname,varargin)
 %   time
 %   dtres   Result of date&time conversion: 0,1
 %   fres    Issues found while reading and interpreting file
-%   nruns   Number of runs stored in file
 %
 % Created:  10/07/16  by Don Hagler
-% Last Mod: 2017aug04-14 by Octavio Ruiz
+% Last Mod: 2017aug04-oct20 by Octavio Ruiz
 %
 
-date = '';   time = '';   dtres = 0;   fres = 0;   nruns = 0;
+date = '';   time = '';   dtres = 0;   fres = 10000;  % Unable to read
+fname_contns_parenthes = false;
 
 if ~mmil_check_nargs(nargin,1), return; end;
-
 
 % fname sometimes is a copy of the original file, signaled by ' (1)' etc. in its name
 % Here I remove those ' (*)' in order to access the original file.
 i1 = strfind(fname,' (');
 if ~isempty(i1)
+    fname_contns_parenthes = true;
     fprintf('\nRequested file name contains parentheses:\n%s\n', fname);
     i2 = strfind(fname,')');
     if ~isempty(i2) && i2 > i1
@@ -37,61 +40,37 @@ if ~isempty(i1)
     end
 end
 
-% check input parameters
-parms = check_input(fname,varargin);
 
-% create output directory
-mmil_mkdir(parms.outdir);
+% Get date and time from eprime file
+date = '';
+time = '';
 
+% Use python script to read file and get task-start date and time
+% % % cmnd0 = './eprime_sprdsht_get.py';
+cmnd0 = '../python/eprime_sprdsht_get.py';
+cmdline = [cmnd0 ' ' fname ' DateTimeDiagnos'];
+[status, cmdout] = system(cmdline);
 
-% Get date and time in eprime (behavioral) data, return first pair in file
-try
-    [info,fres] = eprime_data_get(parms);
-    
-    % Select first date&time pair, to be returned
-    date = info{1,1};
-    time = info{1,2};
-    % Check that strings date and time contain valid date and time information;
-    % if any of the next conversions fail, we'll go to catch and keep dtres = 0
-    datenum(date);
-    datenum(time);
-
-    % Find number of unique date&time combinations in file,
-    % indicative of number of runs in session
-    ddtt = cell2table(info);
-    nruns = size( unique(ddtt), 1 );
-    
-    % If arrived here, everything is right
-    dtres = 1;
-
-catch err
-    fprintf('%s\n- Unable to get timestamps, or invalid values -\n', fname);
-    fprintf('%s\n', err.message);
-    date, time, fres
-
+% Parse returned information into date, time, and file diagnostics
+if ~status
+    [datime,fres_s] = strtok(cmdout, ',');
+    fres = str2num(fres_s);
+    try
+        date = datestr( datime, 'yyyymmdd');
+        time = datestr( datime, 'HHMMSS');
+        % If arrived here, everything is right
+        dtres = 1;
+    catch err
+        fprintf('%s\n- Unable to get datetime -\n', fname);
+        fprintf('%s\n', err.message);
+        date, time, fres
+    end
+else
+    fres = 1000;
+    fprintf('%s\n- Unable to read or interpret file -\n', fname);
 end
 
-return
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function parms = check_input(fname,options)
-  parms = mmil_args2parms(options,{...
-    'fname',fname,[],...
-    ...
-    'outdir',pwd,[],...
-    'outstem',[],[],...
-    'forceflag',true,[false true],...
-    ...
-    'colnames',  {'SessionDate','SessionTime'},[],...
-    'fieldnames',{'date','time'},[],...
-  });
-
-  if ~exist(parms.fname,'file')
-    error('file %s not found',parms.fname);
-  end;
-  [fdir,fstem,fext] = fileparts(parms.fname);
-  if isempty(parms.outstem)
-    parms.outstem = fstem;
-  end;
-return;
+if fname_contns_parenthes
+    % Update fres to reflect presence of parentheses in file name
+    fres = fres + 100000;  % diagnos.number added here must be different from those returned by eprime_sprdsht_get.py
+end
--- readme.md
+++ readme.md
@@ -1,38 +1,31 @@
 ## E-Prime data get
 
-The ABCD project is using E-Prime to run behavioral tests connected to MRI scans. E-prime produces EDAT2 (binary) files with the behavioral results. EDAT2 files are converted, at the acquisition site, into ASCII TAB-separated (preferred) or CSV files (extensions .txt and .csv, resepectively). We have observed several types of errors occuring during this translation.
+The ABCD project is using E-Prime to run behavioral tests during fMRI scans. E-Prime produces EDAT2 (binary) files with the behavioral results. EDAT2 files are converted, at the acquisition site, into ASCII TAB-separated (preferred) or CSV files (extensions .txt and .csv, resepectively). We have observed several types of errors originated during this conversion.
 
-This project tries to extract information from a given ASCII E-prime file, despite known encoding issues.
-
-The project consists of the Matlab function eprime_data_get.m.
-This function returns a subset of columns from an eprime file after tring to repair encoding issues, and reports the issue.
+eprime_sprdsht_get.py extracts information from a given ASCII E-prime file, fixing known encoding issues. It can return information about an E-Prime file (experiment date and time, file diagnositics), or rewrite a clean version of the E-Prime file.
 
 
 ### Usage
 
 ```
-[vals,result] = eprime_data_get(parms)
-%
-% Gets a subset of the columns present in an eprime file fixing encoding issues.
-%
-% Required input:
-%    parms.fname     name of file to read
-%    parms.columns   strucutre with names of columns to be read
+eprime_spreadsheet_read.py  in_file  option  [out_file (without .ext)]
 % 
-% Output
-%    vals    requested columns
-%    result  integer that summarizes the result of reading and interpreting an eprime file.
-
-The value of result is formed by adding the following numbers:
-  0  No issues
-  1  Unable to import file: imported table has one column
-  2  Imported spreadsheet contains rows with different number of cells;
-     the difference is small (1 or 2) and last columns were removed in that number
-  4  Best guess of colum names includes empty cells
-
+% Options:
+%   Encoding         Returns encoding, file format, and row organization of input file
+%   Summary          As Encoding, plus a summary of column names and last row
+%   DateTime         Returns ISO starting date and time of experiment reported in input file
+%   DateTimeDiagnos  As DateTime, plus a file diagnostic code formed by summing:
+%     Encoding:                        1,      2  -->  ['utf-8','utf-16']
+%     Separator:                      10,     20  -->  [Tab,   Comma]
+%     Quoted rows:                     0,    100  -->  [False, True ]
+%     Start_time_info:                 0,   1000  -->  [Found, Unable to extract]
+%     File read:                       0,  10000  -->  [Yes,   Unable to read]
+%     Filename contains '()':             100000  -->  [Yes]   (Not checked by this function)
+%     Experiment and filename mismatch:  1000000  -->  [Yes]
+%   ExportFile    Save a tab-separated output file with depurated contents of input file; we'll make extension = '.txt'
 ```
 
 
 ### Example
 
-eprime_datetime_get.m   Uses eprime_data_get to extract timestamps from an E-prime data file.
+eprime_datetime_get.m   Uses eprime_data_get to extract the date and time of an E-Prime experiment.
